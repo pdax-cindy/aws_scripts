550e4d3dd967e6d827c54189cdcdde51dee8ea77
pdax-cindy <102347713+pdax-cindy@users.noreply.github.com>
2023-01-05 15:50:04 +0800

    Synchronizing job pdax-Dev-data-infra-txn-master-trades-buy-and-sell-reports-job from AWS Glue at 2023/01/05 07:50:04

--- scripts_new/pdax-Dev-data-infra-txn-master-trades-buy-and-sell-reports-job.py
+++ scripts_new/pdax-Dev-data-infra-txn-master-trades-buy-and-sell-reports-job.py
@@ -103,6 +103,7 @@ cast(fee as decimal(19,6)) as fee,
 channel,
 producttype,
 tradedccy,
+'market' as order_type,
 process_date_timestamp,
 p_date
 FROM(
@@ -230,6 +231,11 @@ osl_debit = spark.sql(query).createOrReplaceTempView("osl_debit")
 start_date, end_date, now_date = get_dates()
 query="""
 SELECT *,
+case when round(fee/php_amount*1000)*10 = 40 then 'limit'
+when round(fee/php_amount*1000)*10 = 50 then 'market'
+when round(fee/a.php_amount*1000)*10 = 0 then 'no_fee'
+else 'other' end as order_type,
+from_utc_timestamp('{}', 'Asia/Manila') as process_date_timestamp,
 to_date(transactiondatetime) as p_date
 FROM(
 SELECT
@@ -250,8 +256,7 @@ CASE WHEN a.transactiontype = 'sell' AND a.credit_ccy in ('PHP','PHPT') then a.f
 end as fee,
 a.channel,
 a.producttype,
-CASE WHEN b.debit_ccy in ('PHPT','PHP') then a.credit_ccy else b.debit_ccy end as tradedccy,
-from_utc_timestamp('{}', 'Asia/Manila') as process_date_timestamp
+CASE WHEN b.debit_ccy in ('PHPT','PHP') then a.credit_ccy else b.debit_ccy end as tradedccy
 FROM
 osl_credit a
 LEFT JOIN
@@ -264,7 +269,7 @@ osl_trades_df = spark.sql(query)
 final_df = osl_trades_df.union(otc_trades_df)
 
 spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
-final_df.repartition(1).write.partitionBy("p_date").mode("overwrite").option("header","true").csv("s3://pdax-data-"+env+"-trxn-pull-staging/master_transaction/trades_buy_and_sell/")
+final_df.repartition(1).write.partitionBy("p_date").mode("overwrite").option("header","true").csv("s3://pdax-data-"+env+"-trxn-pull-staging/master_transaction/trades_buy_and_sell/update/")
 
 data_dynamicframe = DynamicFrame.fromDF(final_df.repartition(1), glueContext, "data_dynamicframe")
 
